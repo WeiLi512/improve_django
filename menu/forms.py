6c8151aea2c5a6566c0ce958806e699c05c289d8
boryslee111 <developer.clear@gmail.com>
2019-08-29 15:58:48 +0800

    skeleton

---
+++ menu/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms.extras.widgets import SelectDateWidget
+
+from .models import Menu, Item, Ingredient
+
+
+class MenuForm(forms.ModelForm):
+
+    class Meta:
+        model = Menu
+        exclude = ('created_date',)
